684762958bddedcff7838f864bfc9ee134446273
alisesabanova <87663568+alisesabanova@users.noreply.github.com>
2021-07-19 20:12:25 +0300

    changes

--- Test.playground/Contents.swift
+++ Test.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*
+ Exercise 2
+ Declare an Array of Int. In the wallet you have only 1x by 5 - 500 EUR.
+ Create a func to calculateCash sum of your cash inside your wallet.
+ Run the func.
+ */
+
+
+var coinsInWallet: [Int] = []
+
+for c in 5...500 {
+    coinsInWallet.insert(c, at: 0)
+}
+
+extension Sequence where Element: Numeric {
+    func sum() -> Element { return reduce(0, +) }
+}
+
+print("Total sum of cash in my wallet is \(coinsInWallet.sum())")
